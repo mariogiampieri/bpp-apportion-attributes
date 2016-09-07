aa1f6644feba056510b151716060cc78857929ec
mariogiampieri <mariogiampieri@gmail.com>
2016-09-07 14:40:05 -0400

    minor updates
    missed a commit so the actual apportioning of popuation is missing...

--- split_population.py
+++ split_population.py
@@ -1,18 +1,18 @@
 import arcpy
 
-arcpy.env.workspace = "F:\_data\google_drive\Biking Public Project\maps"
+arcpy.env.workspace = "C:\Users\mario\Documents/bpp\maps"
 arcpy.env.overwriteOutput = True
 
-census_blocks = "F:\_data\google_drive\Biking Public Project\maps/nyc_census_blocks_copy.shp"
-precincts = "F:\_data\google_drive\Biking Public Project\maps\NYC_precincts_utm18_copy.shp"
+census_blocks = "nyc_census_blocks_copy.shp"
+precincts = "NYC_precincts_utm18_copy.shp"
 census_lyr = "census_lyr"
 precincts_lyr = "precincts_lyr"
-c_diss_lyr = "cen_diss_lyr.shp"
+c_diss_lyr = "cen_diss_lyr_w.shp"
 
 arcpy.MakeFeatureLayer_management(precincts, precincts_lyr)
 arcpy.MakeFeatureLayer_management(census_blocks, census_lyr)
 pcursor = arcpy.da.SearchCursor(precincts_lyr, ["OID@", "precinct"])
-ccursor = arcpy.da.SearchCursor(census_lyr, ["precinct", "POP10"])
+ccursor = arcpy.da.SearchCursor(census_lyr, ["precinct", "POP10", "DP0110011"])
 num_p = arcpy.GetCount_management(precincts_lyr)
 
 for row in pcursor:
@@ -39,6 +39,6 @@ for row in pcursor:
 # sum and then add population to precinct polygon - dissolve based on precinct ID and then table join to precinct shape
 arcpy.SelectLayerByAttribute_management(sel_cb, "CLEAR_SELECTION")
 print "dissolving..."
-arcpy.Dissolve_management(sel_cb, c_diss_lyr, dissolve_field="precinct", statistics_fields=[["POP10", 'SUM']]) #
+arcpy.Dissolve_management(sel_cb, c_diss_lyr, dissolve_field="precinct", statistics_fields=[["POP10", 'SUM'], ["DP0110011", 'SUM']])
 
 # join dissolved pop counts to precinct poly based on precinct field attribute
